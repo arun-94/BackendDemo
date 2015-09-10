c7302fc229c7ca22b9ed54ca4589198e6ad1e8a0
arun-94 <arunswaminathan94@gmail.com>
2015-09-11 00:39:42 +0530

    -Dagger Test Files

---
+++ app/src/main/java/com/tisser/puneet/tisserartisan/Component/ActivityComponent.java
@@ -0,0 +1,18 @@
+/*
+package com.tisser.puneet.tisserartisan.Component;
+
+import android.app.Activity;
+
+import com.tisser.puneet.tisserartisan.Module.ActivityModule;
+import com.tisser.puneet.tisserartisan.Scope.PerActivity;
+
+import dagger.Component;
+
+@PerActivity
+@Component(dependencies = ApplicationComponent.class, modules = ActivityModule.class)
+public interface ActivityComponent
+{
+    //exposed to sub-graphs
+    Activity activity();
+}
+*/
---
+++ app/src/main/java/com/tisser/puneet/tisserartisan/Component/ApplicationComponent.java
@@ -0,0 +1,22 @@
+/*
+package com.tisser.puneet.tisserartisan.Component;
+
+import android.content.Context;
+
+import com.tisser.puneet.tisserartisan.Activity.BaseActivity;
+import com.tisser.puneet.tisserartisan.Module.ApplicationModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = ApplicationModule.class)
+public interface ApplicationComponent
+{
+    void inject(BaseActivity baseActivity);
+
+    //Exposed to sub-graphs
+    Context context();
+}
+*/
