50267f03d90fd437cd3acb9e3779e45f6f0652b8
PuneetKohli <punkohl@gmail.com>
2015-10-23 23:58:38 +0530

    - minor

--- app/src/main/java/com/tisser/puneet/tisserartisan/UI/Activity/ProfileActivity.java
+++ app/src/main/java/com/tisser/puneet/tisserartisan/UI/Activity/ProfileActivity.java
@@ -12,6 +12,8 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.AppBarLayout;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -218,6 +220,34 @@ public class ProfileActivity extends BaseActivity
                 profileImage.setImageBitmap(bm);
             }
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_profile_picture)
+        {
+            changeImage();
+            return true;
+        }
+        else if (id == android.R.id.home)
+        {
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
